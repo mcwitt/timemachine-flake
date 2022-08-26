ab3514f028cd04c9d0ebb472f6148d7697062449
Matt Wittmann <mwittmann@relaytx.com>
2022-08-26 16:17:41 +0000

    Add test deps to passthru

--- timemachine/default.nix
+++ timemachine/default.nix
@@ -31,67 +31,74 @@
 , timemachine-src
 }:
 
-buildPythonPackage {
-  name = "timemachine";
-  src = timemachine-src;
+let
+  timemachine = buildPythonPackage {
+    name = "timemachine";
+    src = timemachine-src;
 
-  nativeBuildInputs = [ addOpenGLRunpath cmake mypy pybind11 ];
+    nativeBuildInputs = [ addOpenGLRunpath cmake mypy pybind11 ];
 
-  buildInputs = [ eigen jaxlib ];
+    buildInputs = [ eigen jaxlib ];
 
-  propagatedBuildInputs = [
-    cudatoolkit
-    grpcio
-    hilbertcurve
-    importlib-resources
-    jax
-    matplotlib
-    networkx
-    numpy
-    openeye-toolkits
-    openmm
-    pymbar
-    pyyaml
-    rdkit
-    scipy
-  ];
+    propagatedBuildInputs = [
+      cudatoolkit
+      grpcio
+      importlib-resources
+      jax
+      matplotlib
+      networkx
+      numpy
+      openeye-toolkits
+      openmm
+      pymbar
+      pyyaml
+      rdkit
+      scipy
+    ];
 
-  checkInputs = [ pytest pytestCheckHook ];
+    checkInputs = [ pytestCheckHook ] ++ timemachine.optional-dependencies.test;
 
-  dontUseCmakeConfigure = true;
+    dontUseCmakeConfigure = true;
 
-  patches = [
-    ./patches/fix-interpreter.patch
-    ./patches/unpin-jax.patch
+    patches = [
+      ./patches/fix-interpreter.patch
+      ./patches/unpin-jax.patch
 
-    (substituteAll {
-      src = ./patches/update-cmake-build.patch;
-      pythonVersion = lib.versions.majorMinor python.version;
-    })
+      (substituteAll {
+        src = ./patches/update-cmake-build.patch;
+        pythonVersion = lib.versions.majorMinor python.version;
+      })
 
-    (substituteAll {
-      src = ./patches/hardcode-version.patch;
-      version = timemachine-src.rev or "dirty";
-    })
-  ];
+      (substituteAll {
+        src = ./patches/hardcode-version.patch;
+        version = timemachine-src.rev or "dirty";
+      })
+    ];
 
-  CMAKE_ARGS = "-DCUDA_ARCH=61";
+    CMAKE_ARGS = "-DCUDA_ARCH=61";
 
-  postFixup = ''
-    find $out -type f \( -name '*.so' -or -name '*.so.*' \) | while read lib; do
-      echo "setting opengl runpath for $lib"
-      addOpenGLRunpath "$lib"
-    done
-  '';
+    postFixup = ''
+      find $out -type f \( -name '*.so' -or -name '*.so.*' \) | while read lib; do
+        echo "setting opengl runpath for $lib"
+        addOpenGLRunpath "$lib"
+      done
+    '';
 
-  pythonImportsCheck = [ "timemachine" ];
+    pythonImportsCheck = [ "timemachine" ];
 
-  doCheck = false; # many tests currently require OE license
+    doCheck = false; # many tests currently require OE license
 
-  meta = with lib; {
-    description = "A high-performance differentiable molecular dynamics, docking and optimization engine";
-    homepage = "https://github.com/proteneer/timemachine";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ mcwitt ];
+    passthru.optional-dependencies.test = [
+      pytest
+      hilbertcurve
+    ];
+
+    meta = with lib; {
+      description = "A high-performance differentiable molecular dynamics, docking and optimization engine";
+      homepage = "https://github.com/proteneer/timemachine";
+      license = licenses.asl20;
+      maintainers = with maintainers; [ mcwitt ];
+    };
   };
-}
+in
+timemachine
