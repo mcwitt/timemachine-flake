f63a39631b1e4050bfa54ae24e6624e0192c903e
Matt Wittmann <mwittmann@relaytx.com>
2023-02-07 14:36:59 -0800

    Update dependencies, misc cleanup and fixes
    - use overridePythonAttrs

--- overlay.nix
+++ overlay.nix
@@ -14,49 +14,49 @@ prev:
 
   python310 = prev.python310.override (old: {
     packageOverrides = final.lib.composeExtensions (old.packageOverrides or (_: _: { }))
-      (pyfinal: pyprev: {
-        black_21_12b0 = (pyprev.black.overrideAttrs (oldAttrs: rec {
+      (pyFinal: pyPrev: {
+        black_21_12b0 = (pyPrev.black.overridePythonAttrs (oldAttrs: rec {
           pname = "black";
           version = "21.12b0";
-          src = pyfinal.fetchPypi {
+          src = pyFinal.fetchPypi {
             inherit pname version;
             hash = "sha256-d7gPaTpWni5SeVhFljTxjfmwuiYluk4MLV2lvkLm8rM=";
           };
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pyfinal.pythonRelaxDepsHook ];
+          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pyFinal.pythonRelaxDepsHook ];
           pythonRelaxDeps = [ "tomli" ];
           doInstallCheck = false;
-        })).override { click = pyfinal.click_8_0_4; };
+        })).override { click = pyFinal.click_8_0_4; };
 
         # can remove when upgrading black
         # https://github.com/psf/black/issues/2964
-        click_8_0_4 = pyprev.click.overrideAttrs (_: rec {
+        click_8_0_4 = pyPrev.click.overridePythonAttrs (_: rec {
           pname = "click";
           version = "8.0.4";
-          src = pyfinal.fetchPypi {
+          src = pyFinal.fetchPypi {
             inherit pname version;
             hash = "sha256-hFjXsSh8X7EoyQ4jOBz5nc3nS+r2x/9jhM6E1v4JCts=";
           };
           doInstallCheck = false;
         });
 
-        hilbertcurve = pyfinal.callPackage ./packages/hilbertcurve.nix { };
+        hilbertcurve = pyFinal.callPackage ./packages/hilbertcurve.nix { };
 
-        openeye-toolkits = pyfinal.callPackage ./packages/openeye-toolkits.nix { };
+        openeye-toolkits = pyFinal.callPackage ./packages/openeye-toolkits.nix { };
 
-        openmm = pyprev.openmm.override {
+        openmm = pyPrev.openmm.override {
           inherit (final) cudaPackages;
           enableCuda = false;
         };
 
-        py3Dmol = pyfinal.callPackage ./packages/py3Dmol.nix { };
+        py3Dmol = pyFinal.callPackage ./packages/py3Dmol.nix { };
 
-        pymbar = pyfinal.callPackage ./packages/pymbar.nix { };
+        pymbar = pyFinal.callPackage ./packages/pymbar.nix { };
 
-        timemachine = pyfinal.callPackage ./packages/timemachine {
+        timemachine = pyFinal.callPackage ./packages/timemachine {
           inherit (final.cudaPackages) cub cuda_cudart cuda_nvcc libcurand thrust;
         };
 
-        mols2grid = pyfinal.callPackage ./packages/mols2grid.nix { };
+        mols2grid = pyFinal.callPackage ./packages/mols2grid.nix { };
       });
   });
 }
