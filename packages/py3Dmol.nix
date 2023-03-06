35802b85ac8c09a26b0ea414f47201975c035ddc
Matt Wittmann <mwittmann@relaytx.com>
2023-03-06 10:27:18 -0800

    Upgrade py3Dmol

--- packages/py3Dmol.nix
+++ packages/py3Dmol.nix
@@ -2,9 +2,9 @@
 
 buildPythonPackage rec {
   pname = "py3Dmol";
-  version = "1.8.1";
+  version = "2.0.1.post1";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-ELswIsXbPl5tZvUdfUEYr6+TlybWVfCHYXCd3wMssx0=";
+    sha256 = "sha256-rdcOz49keXCSXrjBBDxcE0OBP6SeYTt38GKOUixBSKw=";
   };
 }
