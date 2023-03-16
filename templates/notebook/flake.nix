71a1ea3b05f554bda740b7d4fb33cbb02ea22b9c
Matt Wittmann <mwittmann@relaytx.com>
2023-03-16 16:22:43 -0700

    Update templates

--- templates/notebook/flake.nix
+++ templates/notebook/flake.nix
@@ -12,20 +12,7 @@
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        overlays =
-          let
-            overlay = _: super: {
-              python3 = super.python3.override (old: {
-                packageOverrides = nixpkgs.lib.composeExtensions (old.packageOverrides or (_: _: { })) (final: _: {
-                  jupyter-black = final.callPackage ./nix/jupyter-black.nix { };
-                });
-              });
-            };
-          in
-          [
-            timemachine-flake.overlays.default
-            overlay
-          ];
+        overlays = [ timemachine-flake.overlays.default ];
       };
 
     in
