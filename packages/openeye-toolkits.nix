b2396db0d0b5525c8e9ae8c3b26150f295a7027e
Matt Wittmann <mwittmann@relaytx.com>
2023-04-18 12:36:06 -0700

    Downgrade openeye-toolkits: 2022.2.0 -> 2020.2.0
    Work around issue with charge generation

--- packages/openeye-toolkits.nix
+++ packages/openeye-toolkits.nix
@@ -9,7 +9,7 @@
 , zlib
 }:
 let
-  version = "2022.2.2";
+  version = "2020.2.2";
 
   mkUrl = pname: "https://pypi.anaconda.org/openeye/simple/${pname}/${version}/${pname}-${version}.tar.gz";
 
@@ -18,15 +18,15 @@ let
       pname = "OpenEye-toolkits-python3-linux-x64";
       src = fetchurl {
         url = mkUrl pname;
-        hash = "sha256-WPtjRaQfGj/pHrPrT2L1SDNbNSnPtDvJGaYD/Uhcztk=";
+        hash = "sha256-DYhwD59IRiyE5HIkNrkaqE9aUAWn8oqSrXq6fx/vcaI=";
       };
     };
 
     x86_64-darwin = rec {
-      pname = "OpenEye-toolkits-python3-osx-universal";
+      pname = "OpenEye-toolkits-python3-osx-x64";
       src = fetchurl {
         url = mkUrl pname;
-        hash = "sha256-EnDRZqdQR/o8wW3hiUaqQQ7b+sdIgSGMIgWK7qAmyVY=";
+        hash = "sha256-MqR4n3a4xVTfUkP1rUtkdkoPC/Lx+pM7eT3fJHZX9Oo=";
       };
     };
   };
@@ -54,6 +54,6 @@ buildPythonPackage rec {
 
   src = fetchurl {
     url = mkUrl pname;
-    hash = "sha256-VGQmSyMJNODuyNcHTgD+xnFI1ylLIHWa21viw67aPvA=";
+    hash = "sha256-zBytId8O1ChPfK0n68b3+/fs7wNI0k08YKKQODMjMOM=";
   };
 }
