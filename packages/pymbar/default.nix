cf35de75c08148f314cd4de146adbb3428f203cb
Matt Wittmann <mwittmann@relaytx.com>
2023-06-05 17:04:34 -0700

    Run pymbar tests, clean

--- packages/pymbar/default.nix
+++ packages/pymbar/default.nix
@@ -1,9 +1,7 @@
 { buildPythonPackage
 , fetchFromGitHub
 , numpy
-, pytest
-, pytest-cov
-, pytest-runner
+, pytestCheckHook
 , scipy
 , six
 }:
@@ -11,13 +9,24 @@
 buildPythonPackage rec {
   pname = "pymbar";
   version = "3.1.0";
+  format = "setuptools";
+
   src = fetchFromGitHub {
     owner = "choderalab";
     repo = "pymbar";
     rev = "refs/tags/${version}";
     sha256 = "sha256-lyUFPvhbVsqLIYRspYd+Mk40/1rCYP2W6ESY/7UDUT4=";
   };
+
   patches = [ ./0001-Remove-deprecated-int-alias.patch ];
-  propagatedBuildInputs = [ numpy scipy six ];
-  checkInputs = [ pytest pytest-cov pytest-runner ];
+
+  propagatedBuildInputs = [
+    numpy
+    scipy
+    six
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 }
